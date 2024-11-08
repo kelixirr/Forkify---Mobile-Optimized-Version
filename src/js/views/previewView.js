05c7500dd163102db1ee47b55c162c3d8fe2a17e
Amritesh <81531722+kelixirr@users.noreply.github.com>
2024-11-08 13:17:01 +0530

    Update previewView.js markup

--- src/js/views/previewView.js
+++ src/js/views/previewView.js
@@ -16,7 +16,7 @@ class PreviewView extends View {
                 <img src="${this._data.image}" alt="${this._data.image}" />
               </figure>
               <div class="preview__data">
-                <h4 class="preview__title">${this._data.title}/h4>
+                <h4 class="preview__title">${this._data.title} </h4>
                 <p class="preview__publisher">${this._data.publisher}</p>
               <div class="preview__user-generated ${
                 this._data.key ? '' : 'hidden'
